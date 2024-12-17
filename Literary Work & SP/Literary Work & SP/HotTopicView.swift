f5656cfd5a2728d0d8545fabaa8f2ffd008ab926
Jasmine <973193460@qq.com>
2024-12-17 14:21:28 +0800

    Changes APP File

--- Literary Work & SP/Literary Work & SP/HotTopicView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HotTopicView.swift
-//  Literary Work & SP
-//
-//  Created by 刘嘉欣 on 12/15/24.
-//
-
-import Foundation
---
+++ Literary Work & SP/Literary Work & SP/RootView.swift
@@ -0,0 +1,8 @@
+//
+//  RootView.swift
+//  Literary Work & SP
+//
+//  Created by 刘嘉欣 on 12/17/24.
+//
+
+import Foundation
